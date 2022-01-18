ac91a91922d5647a276e3474b539d146307b6002
KHardik1698 <KHardik1698@users.noreply.github.com>
2022-01-18 13:13:36 +0000

    deploy: fd213a37b1d2b9eb9bef344be56412cd48827e85

---
+++ precache-manifest.ca749dbcd496a8b4f47412b99fc95f7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d80751c92f9e6a24c895cf259658816",
+    "url": "https://khardik1698.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "7a045bcb589f7738f369",
+    "url": "https://khardik1698.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "8632bf4c7cc8401c7615",
+    "url": "https://khardik1698.github.io/task-management-frontend/static/js/2.18e50a6a.chunk.js"
+  },
+  {
+    "revision": "7a045bcb589f7738f369",
+    "url": "https://khardik1698.github.io/task-management-frontend/static/js/main.00d64188.chunk.js"
+  },
+  {
+    "revision": "cd15ebed3b8102534067",
+    "url": "https://khardik1698.github.io/task-management-frontend/static/js/runtime~main.6a5706f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://khardik1698.github.io/task-management-frontend/precache-manifest.ca749dbcd496a8b4f47412b99fc95f7b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://khardik1698.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
